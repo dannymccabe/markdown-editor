9bbdaa7bcdd11ce9884d989f651c316ab6a50a9e
Danny McCabe <94184743+darnomccode@users.noreply.github.com>
2022-06-08 20:24:49 -0300

    Add files via upload
    Final push

---
+++ README.md
@@ -0,0 +1,8 @@
+# About the markdown Editor
+
+![alt](markdown-screenshot.png)
+
+Built in ReactJs using the Remarkable package, this markdown editor is a conversion of the markdown example on the ReactJs website using function components instead of class components.
+
+## See it live
+https://creative-pudding-4c0aca.netlify.app/
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import { Remarkable } from "remarkable"
+
+const md = new Remarkable()
+
+export default function App() {
+  const [text, setText] = useState("")
+
+  return (
+    <main className="px-5 py-10 text-gray-900 md:max-w-3xl md:mx-auto">
+      <h1 className="font-bold text-center text-4xl xl:text-6xl mb-5">
+        Markdown editor
+      </h1>
+
+      <article className="flex flex-col items-start justify-center rounded">
+        <label htmlFor="markdown" className="mb-3 mt-5 block">
+          Type in something to see the magic
+        </label>
+        <textarea
+          name="textarea"
+          id="markdown"
+          cols="30"
+          rows="10"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="w-full text-gray-900 tracking-wide p-5 rounded shadow"
+          placeholder="Type in something"
+        ></textarea>
+      </article>
+
+      <h3 className="mt-5 font-bold tracking-wide text-xl">Output</h3>
+      <div
+        className="content"
+        dangerouslySetInnerHTML={{ __html: md.render(text) }}
+      ></div>
+    </main>
+  )
+}
